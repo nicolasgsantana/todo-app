5bdb84459144001bced3f03dc1af4e4ac9d370b3
nicolasgsantana <nicolas07san@gmail.com>
2024-03-05 16:34:51 -0300

    Import task.js and list.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 import '../css/style.css';
+import { createList } from './list';
+import { createTask } from './task';
+
+const firstTask = createTask("Learn", "Play something", "anything", "High");
+const firstList = createList("Home");
+
+firstList.addTask(firstTask);
+firstList.addTask(firstTask);
+firstList.addTask(firstTask);
+console.log(firstList.getTasks());
 
