3fa989f1831e2a49ddcc27597cfbb76d922d65d7
nicolasgsantana <nicolas07san@gmail.com>
2024-03-12 16:43:38 -0300

    Tests with input date

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,20 @@
 import '../css/style.css';
 import { createList } from './list';
 import { createTask } from './task';
+import { formatDate } from './dateManager';
 
-const firstTask = createTask("Learn", "Play something", "anything", "High");
-const firstList = createList("Home");
+const first = createTask("Code", "Practice CSS", "12-03-2024", "High");
+const second = createTask("Learn", "Play the flute", "15-04-2024", "High");
+const third = createTask("Run", "Five Miles", "12-03-2024", "High");
+const list = createList("Home");
 
-firstList.addTask(firstTask);
-firstList.addTask(firstTask);
-firstList.addTask(firstTask);
-console.log(firstList.getTasks());
+list.addTask(first);
+list.addTask(second);
+list.addTask(third);
+console.log(list.getTasks());
+console.log(formatDate("2024-03-12"));
 
+const dateInput = document.getElementById("date");
+dateInput.addEventListener("input", () => {
+    console.log(dateInput.value);
+})
