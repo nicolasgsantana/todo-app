66b2f6ed29f8768460eeadec44bf8efe5d4f553a
nicolasgsantana <nicolas07san@gmail.com>
2024-03-13 16:29:43 -0300

    Add more test stuff

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,18 @@ import { createList } from './list';
 import { createTask } from './task';
 import { formatDate } from './dateManager';
 
-const first = createTask("Code", "Practice CSS", "12-03-2024", "High");
-const second = createTask("Learn", "Play the flute", "01-05-2024", "High");
-const third = createTask("Run", "Five Miles", "02-04-2024", "High");
+const first = createTask("Code", "Practice CSS", "12-03-2024", false);
+const second = createTask("Learn", "Play the flute", "12-03-2024", true);
+const third = createTask("Run", "Five Miles", "02-04-2024", true);
+const fourth = createTask("Dance", "Jump up and down idk", "12-03-2024", true);
+const fifth = createTask("Read", "Just pick up a book", "12-03-2024", true);
 const list = createList("Home");
 
 list.addTask(first);
 list.addTask(second);
 list.addTask(third);
+list.addTask(fourth);
+list.addTask(fifth);
 console.log(list.getTasks());
 console.log(formatDate("2024-03-12"));
 
