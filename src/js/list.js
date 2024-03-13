5309d57c83fe92cefaea052a989c07349aa6f901
nicolasgsantana <nicolas07san@gmail.com>
2024-03-12 22:56:02 -0300

    Add date sorting

--- src/js/list.js
+++ src/js/list.js
@@ -1,11 +1,33 @@
+import { parseDate, sortDates, formatToISO } from "./dateManager";
+
 export function createList(name) {
-    const tasks = {};
+    let tasks = {};
 
     const addTask = task => {
         if (task.dueDate in tasks)
             tasks[task.dueDate].push(task);
         else
             tasks[task.dueDate] = [task];
+
+        sortTasks();
+    }
+
+    const sortTasks = () => {
+        const dates = [];
+        for (const property in tasks) {
+            dates.push(parseDate(property));
+        }
+
+        const sortedDates = sortDates(dates);
+
+        const sortedTasks = {}
+
+        sortedDates.forEach(date => {
+            const formattedDate = formatToISO(date);
+            sortedTasks[formattedDate] = tasks[formattedDate];
+        });
+
+        tasks = sortedTasks;
     }
 
     const getTasks = () => {
