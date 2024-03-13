58157ea057055cb36fd7064dc27325db13e919b1
nicolasgsantana <nicolas07san@gmail.com>
2024-03-12 22:54:38 -0300

    Add parseDate, sortDates and formatToISO functions

--- src/js/dateManager.js
+++ src/js/dateManager.js
@@ -1,5 +1,17 @@
-import { format, parseISO } from "date-fns";
+import { format, parseISO, parse, compareAsc } from "date-fns";
 
 export function formatDate(date) {
     return format(parseISO(date), "MMM d");
+}
+
+export function parseDate(date) {
+    return parse(date, "dd-MM-y", new Date());
+}
+
+export function sortDates(dateList) {
+    return dateList.toSorted(compareAsc);
+}
+
+export function formatToISO(date) {
+    return format(date, "dd-MM-y");
 }
